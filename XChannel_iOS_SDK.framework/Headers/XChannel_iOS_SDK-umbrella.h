813f0e407ec1773ef4aed9b9c91df8310aa17923
Usha Vydyanathan <ushavydya@Ushas-MacBook-Pro.local>
2016-06-28 20:52:53 -0700

    Removed unused imports

--- XChannel_iOS_SDK.framework/Headers/XChannel_iOS_SDK-umbrella.h
+++ XChannel_iOS_SDK.framework/Headers/XChannel_iOS_SDK-umbrella.h
@@ -1,6 +1,6 @@
 #import <UIKit/UIKit.h>
 
-#import "Configuration.h"
+
 #import "XCBeacon.h"
 #import "XCBeaconDeal.h"
 #import "XCBeaconRegion.h"
@@ -8,12 +8,7 @@
 #import "XCCampaignSocial.h"
 #import "XChannel.h"
 #import "XCUser.h"
-#import "NSData+Base64.h"
-#import "XCCommonUtils.h"
-#import "XCConfiguration.h"
-#import "XChannelGlobals.h"
-#import "XCLocationManager.h"
-#import "XCServerProxy.h"
+
 
 FOUNDATION_EXPORT double XChannel_iOS_SDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char XChannel_iOS_SDKVersionString[];
